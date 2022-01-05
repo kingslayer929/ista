0e0802fe645ac026bbbda3285eb4fee6485261b7
kingslayer929 <percylin0929@gmail.com>
2022-01-05 16:34:05 +0800

    C_DT14

---
+++ C_DT14.cpp
@@ -0,0 +1,35 @@
+#include <utility>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int c, cnt = 0;
+    char str[105];
+    scanf("%d%s", &c, str);
+    pair<char, int> tree[100];
+    for (char *p = strtok(str, ",()\n"); p != NULL; p = strtok(NULL, ",()\n")){
+        tree[cnt].first = *p;
+        p = strtok(NULL, ",()\n");
+        tree[cnt].second = atoi(p);
+        cnt++;
+    }
+    int i = 1;
+    for (;; i++){
+        if(abs(tree[(i-1)/2].second - tree[i].second) <= c){
+            printf("%c%c", tree[(i - 1) / 2].first, tree[i].first);
+            i++;
+            break;
+        }
+    }
+    for (; i < cnt; i++){
+        if(abs(tree[(i-1)/2].second - tree[i].second) <= c){
+            printf(" %c%c", tree[(i - 1) / 2].first, tree[i].first);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
